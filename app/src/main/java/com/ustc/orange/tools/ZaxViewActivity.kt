5eaa062fb9044ef9ccac2ef63f8cd54828a40d15
chengzhi <chengzhi@kuaishou.com>
2021-08-21 15:57:41 +0800

    [zax]建立测试自定义view页面

---
+++ app/src/main/java/com/ustc/orange/tools/ZaxViewActivity.kt
@@ -0,0 +1,12 @@
+package com.ustc.orange.tools
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ZaxViewActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_zax_view)
+  }
+}
