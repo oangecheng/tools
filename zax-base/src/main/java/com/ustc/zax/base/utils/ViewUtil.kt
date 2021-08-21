1f1f4b6889b2a9c1332960bab3cfd45425b638ce
chengzhi <chengzhi@kuaishou.com>
2021-08-21 19:19:58 +0800

    [zax]新增几个工具方法

--- zax-base/src/main/java/com/ustc/zax/base/utils/ViewUtil.kt
+++ zax-base/src/main/java/com/ustc/zax/base/utils/ViewUtil.kt
@@ -2,6 +2,13 @@ package com.ustc.zax.base.utils
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
 import com.ustc.zax.base.GlobalConfig
 
 /**
@@ -21,6 +28,32 @@ object ViewUtil {
 
   fun dp2px(dp: Int): Int {
     return dp2px(dp.toFloat())
+  }
+
+  fun sp2px(sp: Float): Int {
+    val fontScale = res().displayMetrics.scaledDensity
+    return (sp * fontScale + 0.5f).toInt()
+  }
+
+  fun sp2px(sp: Int): Int {
+    return sp2px(sp.toFloat())
+  }
+
+  fun dimen(@DimenRes dimenRes: Int): Int {
+    return res().getDimension(dimenRes).toInt()
+  }
+
+  fun drawable(@DrawableRes drawableRes: Int): Drawable? {
+    return ContextCompat.getDrawable(context(), drawableRes);
+  }
+
+  @ColorInt
+  fun color(@ColorRes colorRes: Int): Int {
+    return ContextCompat.getColor(context(), colorRes)
+  }
+
+  fun string(@StringRes stringRes: Int): String {
+    return res().getString(stringRes)
   }
 
   fun res(): Resources {
