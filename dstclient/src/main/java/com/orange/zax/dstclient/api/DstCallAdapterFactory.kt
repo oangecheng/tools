93c3a879a69076cb17d2cddd66d1d9d45ce450fd
chengzhi <chengzhi@kuaishou.com>
2023-09-11 11:01:56 +0800

    打包修改jdk版本

--- dstclient/src/main/java/com/orange/zax/dstclient/api/DstCallAdapterFactory.kt
+++ dstclient/src/main/java/com/orange/zax/dstclient/api/DstCallAdapterFactory.kt
@@ -15,7 +15,7 @@ class DstCallAdapterFactory : CallAdapter.Factory() {
   private var lastTime = 0L
   companion object {
     // 请求间隔
-    private const val INTERVAL_LIMIT = 2000L
+    private const val INTERVAL_LIMIT = 500L
   }
 
   override fun get(
